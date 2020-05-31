4dc33522bc36711096d91d8122c6920bf3b3aa6a
Navasardian Michael <navasardianmichael@gmail.com>
2020-06-01 02:41:45 +0400

    Updates

---
+++ precache-manifest.64a8912c251c760e83d1ad020e6a9335.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d73f486889051b26a54c02474955c2",
+    "url": "/Sonrisa-users-app/index.html"
+  },
+  {
+    "revision": "735dbb34a9b7c2e6913c",
+    "url": "/Sonrisa-users-app/static/css/main.6afd72aa.chunk.css"
+  },
+  {
+    "revision": "ade5a79437024b026ad3",
+    "url": "/Sonrisa-users-app/static/js/2.fe4448cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Sonrisa-users-app/static/js/2.fe4448cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "735dbb34a9b7c2e6913c",
+    "url": "/Sonrisa-users-app/static/js/main.f39b2099.chunk.js"
+  },
+  {
+    "revision": "5f9e8c51ac89a5b0937e",
+    "url": "/Sonrisa-users-app/static/js/runtime-main.13702c12.js"
+  }
+]);
